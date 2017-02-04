9f37f1dd024d92c2955e6a9ef8f85828cb79571c
Indika Chamara (IC) Ranasinghe <ic@iceda.net>
2017-02-03 22:56:18 -0800

    Converted code to Power BI CLI APIs from Dev Tools
    Converted Tachometer code to CLI v 1.4 compliant code.

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var Tachometer1474636471549 = {
+        name: 'Tachometer1474636471549',
+        displayName: 'Tachometer',
+        class: 'Tachometer',
+        version: '2.0.0',
+        apiVersion: '1.4.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.Tachometer1474636471549.Tachometer(options),
+        custom: true
+    };
+}
